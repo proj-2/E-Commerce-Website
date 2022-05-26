2d21baf1b38661824b85fc7426a5750697ce9968
Brennan <merrett.b@gmail.com>
2022-05-26 13:28:25 -0400

    began working with handlebars, added views folder and files

---
+++ public/javascript/login.js
@@ -0,0 +1,27 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/search');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  
+  
