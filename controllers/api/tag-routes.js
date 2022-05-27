5398a1272dae5ea73ad30f6bf13cf7f66a372567
Charanvir <charanvir123@gmail.com>
2022-05-27 00:19:33 -0400

    created crud routes for Tag data

--- controllers/api/tag-routes.js
+++ controllers/api/tag-routes.js
@@ -21,4 +21,81 @@ router.get('/', (req, res) => {
         })
 });
 
+router.get("/:id", (req, res) => {
+    Tag.findOne({
+        attributes: ['id', 'name'],
+        include: [
+            {
+                model: Product,
+                attributes: ['id', 'name', 'description', 'price', 'SKU'],
+                through: ProductTag,
+                as: 'products'
+            }
+        ],
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(tagData => {
+            if (!tagData) {
+                res.status(404).json({ message: 'No category found with this id' })
+                return;
+            }
+            res.json(tagData)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+});
+
+router.post("/", (req, res) => {
+    Tag.create({
+        name: req.body.name
+    })
+        .then(tagData => res.json(tagData))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+});
+
+router.put("/:id", (req, res) => {
+    Tag.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(tagData => {
+            if (!tagData) {
+                res.status(404).json({ message: 'No category found with this id' })
+                return;
+            }
+            res.json(tagData)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+});
+
+router.delete("/:id", (req, res) => {
+    Tag.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(tagData => {
+            if (!tagData) {
+                res.status(404).json({ message: 'No category found with this id' })
+                return;
+            }
+            res.json(tagData)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
 module.exports = router
