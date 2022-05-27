2277fa69b779d763c443f6fbc97f41f6513368ca
Charanvir <charanvir123@gmail.com>
2022-05-27 15:43:09 -0400

    rendering startup page, login and logout button functionality added

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
+const startUpRoutes = require("./startUpRoutes")
 
 router.use("/api", apiRoutes);
+router.use("/", startUpRoutes)
 
 module.exports = router;
