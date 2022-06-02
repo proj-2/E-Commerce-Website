d06bc04b5815585a4ef6132e76fe3390955e919b
Charanvir <charanvir123@gmail.com>
2022-06-02 09:58:58 -0400

    created additional seed data

--- seeds/product-tag-seeds.js
+++ seeds/product-tag-seeds.js
@@ -18,12 +18,20 @@ const productTagData = [
         tag_id: 12
     },
     {
+        product_id: 2,
+        tag_id: 17
+    },
+    {
         product_id: 3,
         tag_id: 3
     },
     {
         product_id: 3,
         tag_id: 15
+    },
+    {
+        product_id: 3,
+        tag_id: 17
     },
     {
         product_id: 4,
@@ -78,6 +86,10 @@ const productTagData = [
         tag_id: 16
     },
     {
+        product_id: 6,
+        tag_id: 17
+    },
+    {
         product_id: 7,
         tag_id: 6
     },
@@ -108,6 +120,58 @@ const productTagData = [
     {
         product_id: 10,
         tag_id: 10
+    },
+    {
+        product_id: 11,
+        tag_id: 1
+    },
+    {
+        product_id: 11,
+        tag_id: 4
+    },
+    {
+        product_id: 12,
+        tag: 4
+    },
+    {
+        product_id: 12,
+        tag: 5
+    },
+    {
+        product_id: 13,
+        tag: 10
+    },
+    {
+        product_id: 12,
+        tag: 6
+    },
+    {
+        product_id: 13,
+        tag: 9
+    },
+    {
+        product_id: 12,
+        tag: 10
+    },
+    {
+        product_id: 13,
+        tag: 3
+    },
+    {
+        product_id: 13,
+        tag: 12
+    },
+    {
+        product_id: 13,
+        tag: 15
+    },
+    {
+        product_id: 13,
+        tag: 17
+    },
+    {
+        product_id: 13,
+        tag: 16
     },
 ]
 
