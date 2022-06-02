d4402396d60566b80e7194ed406d817db325ecc9
Feroz Zargar <Ferozzargar@hotmail.com>
2022-06-02 10:21:23 -0400

    updated checkout route

--- controllers/checkoutRoutes.js
+++ controllers/checkoutRoutes.js
@@ -13,55 +13,27 @@ const validation = require("../utils/validation");
 
 router.get("/", validation, (req, res) => {
   console.log(req.session);
-  Order.findAll({
+  User.findAll({
     where: {
-      user_id: req.session.user_id,
+      id: req.session.user_id,
     },
-    // attributes: [
-    //   "id",
-    //   "name",
-    //   "description",
-    //   "price",
-    //   "SKU",
-    //   "origin",
-    //   "category_id",
-    //   "user_id",
-    //   "shipping_id",
-    //   "stock",
-    //   "length",
-    //   "width",
-    //   "height",
-    //   "dimension_units",
-    //   "weight",
-    //   "weight_units",
-    // ],
-    // include: [
-    //   {
-    //     model: User,
-    //     attributes: ["id", "first_name", "last_name", "currency"],
-    //   },
-    //   {
-    //     model: Category,
-    //     attributes: ["id", "category_name"],
-    //   },
-    //   {
-    //     model: ShippingProvider,
-    //     attributes: ["id", "shipping_name"],
-    //   },
-    //   {
-    //     model: Tag,
-    //     attributes: ["id", "name"],
-    //     through: ProductTag,
-    //     as: "tags",
-    //   },
-    // ],
+    attributes: ["id", "email"],
+    include: {
+      model: Product,
+      attributes: ["id", "name", "price"],
+      through: Order,
+      as: "product_order",
+    },
   })
-    .then((productData) => {
-      const products = productData.map((product) =>
-        product.get({ plain: true })
-      );
-      console.log(products);
-      res.render("checkout", { products, loggedIn: true });
+    .then((orderData) => {
+      const initialOrderData = orderData.map((order) =>
+        order.get({ plain: true })
+      )[0].product_order;
+      const orders = initialOrderData.map((order) => ({
+        ...order,
+      }));
+      console.log(orderData);
+      res.render("checkout", { orders, loggedIn: true });
     })
     .catch((err) => {
       console.log(err);
