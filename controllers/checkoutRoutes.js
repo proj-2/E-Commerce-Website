b16e0406cfdbe2c08ae520048aa2eb2d09013624
Charanvir <charanvir123@gmail.com>
2022-06-02 11:41:51 -0400

    added validation to checkout form

--- controllers/checkoutRoutes.js
+++ controllers/checkoutRoutes.js
@@ -17,7 +17,7 @@ router.get("/", validation, (req, res) => {
     where: {
       id: req.session.user_id,
     },
-    attributes: ["id", "email"],
+    attributes: ["id", "email", 'currency'],
     include: {
       model: Product,
       attributes: ["id", "name", "price"],
@@ -29,11 +29,19 @@ router.get("/", validation, (req, res) => {
       const initialOrderData = orderData.map((order) =>
         order.get({ plain: true })
       )[0].product_order;
+      const getCurrency = orderData.map((order) =>
+        order.get({ plain: true })
+      )
       const orders = initialOrderData.map((order) => ({
         ...order,
       }));
-      console.log(orders);
-      res.render("checkout", { orders, loggedIn: true });
+      let totalPrice = 0
+      for (let i = 0; i < orders.length; i++) {
+        totalPrice += orders[i].price
+      }
+      let price = totalPrice.toFixed(2)
+      let currency = getCurrency[0].currency
+      res.render("checkout", { currency, price, loggedIn: true });
     })
     .catch((err) => {
       console.log(err);
