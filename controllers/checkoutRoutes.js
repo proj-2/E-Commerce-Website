49a4c041aea7cba8f1d13657239c56fff5a581ec
Feroz Zargar <Ferozzargar@hotmail.com>
2022-06-02 10:10:49 -0400

    testing checkout route

--- controllers/checkoutRoutes.js
+++ controllers/checkoutRoutes.js
@@ -6,59 +6,61 @@ const {
   Tag,
   ShippingProvider,
   ProductTag,
+  Order,
 } = require("../models");
 
 const validation = require("../utils/validation");
 
 router.get("/", validation, (req, res) => {
   console.log(req.session);
-  Product.findAll({
+  Order.findAll({
     where: {
       user_id: req.session.user_id,
     },
-    attributes: [
-      "id",
-      "name",
-      "description",
-      "price",
-      "SKU",
-      "origin",
-      "category_id",
-      "user_id",
-      "shipping_id",
-      "stock",
-      "length",
-      "width",
-      "height",
-      "dimension_units",
-      "weight",
-      "weight_units",
-    ],
-    include: [
-      {
-        model: User,
-        attributes: ["id", "first_name", "last_name", "currency"],
-      },
-      {
-        model: Category,
-        attributes: ["id", "category_name"],
-      },
-      {
-        model: ShippingProvider,
-        attributes: ["id", "shipping_name"],
-      },
-      {
-        model: Tag,
-        attributes: ["id", "name"],
-        through: ProductTag,
-        as: "tags",
-      },
-    ],
+    // attributes: [
+    //   "id",
+    //   "name",
+    //   "description",
+    //   "price",
+    //   "SKU",
+    //   "origin",
+    //   "category_id",
+    //   "user_id",
+    //   "shipping_id",
+    //   "stock",
+    //   "length",
+    //   "width",
+    //   "height",
+    //   "dimension_units",
+    //   "weight",
+    //   "weight_units",
+    // ],
+    // include: [
+    //   {
+    //     model: User,
+    //     attributes: ["id", "first_name", "last_name", "currency"],
+    //   },
+    //   {
+    //     model: Category,
+    //     attributes: ["id", "category_name"],
+    //   },
+    //   {
+    //     model: ShippingProvider,
+    //     attributes: ["id", "shipping_name"],
+    //   },
+    //   {
+    //     model: Tag,
+    //     attributes: ["id", "name"],
+    //     through: ProductTag,
+    //     as: "tags",
+    //   },
+    // ],
   })
     .then((productData) => {
       const products = productData.map((product) =>
         product.get({ plain: true })
       );
+      console.log(products);
       res.render("checkout", { products, loggedIn: true });
     })
     .catch((err) => {
