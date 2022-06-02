c5eed95be9eae6dc97d5bf2287306ef7086d4edf
Feroz Zargar <Ferozzargar@hotmail.com>
2022-06-02 10:02:09 -0400

    checkout route added

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
-const startUpRoutes = require("./startUpRoutes")
-const profileRoutes = require("./profileRoutes")
+const startUpRoutes = require("./startUpRoutes");
+const profileRoutes = require("./profileRoutes");
+const checkoutRoutes = require("./checkoutRoutes");
 
 router.use("/api", apiRoutes);
-router.use("/profile", profileRoutes)
-router.use("/", startUpRoutes)
+router.use("/profile", profileRoutes);
+router.use("/", startUpRoutes);
+router.use("/checkout", checkoutRoutes);
 
 module.exports = router;
