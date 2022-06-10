675b8c556a3140b141a83a659e3ae93215224d30
Yurippe <yurichikawa1992@gmail.com>
2022-06-10 15:37:46 -0400

    refactor search.js in public/javascript folder

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,11 +1,12 @@
+// search by category
 async function searchFormHandler(event) {
   event.preventDefault();
 
+  // get category_id and prefer_currency info
   const category_id = document.querySelector("#search-category").value.trim();
   const prefer_cur = document.querySelector('#pref-cur').value.trim();
-  console.log(category_id)
-  console.log(prefer_cur);
 
+  // fetch curRate api to apply currency rate when rendering info to search result handlebars
   const response = await fetch('/api/curRate', {
     method: 'POST',
     body: JSON.stringify({
@@ -14,19 +15,23 @@ async function searchFormHandler(event) {
     headers: { 'Content-Type': 'application/json' }
   });
 
+  // if the response is okay, go to /search/category/:id to render the result to search result handlebars
   if (response.ok) {
-    document.location.replace(`search/category/${category_id}`)
+    window.location.replace(`/search/category/${category_id}`);
   } else {
     alert(response.statusText);
   }
 }
+
+// search by tag
 async function searchTagFrom(event) {
   event.preventDefault();
 
+  // get tag id and prefer currency info
   const tag_id = document.querySelector("#search-tag").value.trim();
   const prefer_cur = document.querySelector('#pref-cur').value.trim();
-  console.log(tag_id)
 
+  // fetch curRate api to apply currency rate when rendering info to search result handlebars
   const response = await fetch('/api/curRate', {
     method: 'POST',
     body: JSON.stringify({
@@ -35,13 +40,13 @@ async function searchTagFrom(event) {
     headers: { 'Content-Type': 'application/json' }
   });
 
+  // if the response is okay, go to /search/tag/:id to render the result to search result handlebars
   if (response.ok) {
-    document.location.replace(`search/tag/${tag_id}`)
+    document.location.replace(`search/tag/${tag_id}`);
   } else {
     alert(response.statusText);
   }
 }
-
 
 document.querySelector('#searchForm').addEventListener('submit', searchFormHandler);
 document.querySelector('#tagSearch').addEventListener('submit', searchTagFrom);
--- public/javascript/single-product.js
+++ public/javascript/single-product.js
@@ -1,3 +1,35 @@
+// add to my order list
+async function orderFunction(event) {
+    event.preventDefault();
+
+    // get product id from the url
+    const product_id = window.location.pathname.split('/')[
+        window.location.pathname.split('/').length - 1
+    ];
+
+    // get modal elements
+    const modal = document.querySelector('#info-modal');
+    const modal_title = document.querySelector('#info-title');
+
+    // fetch order api to add to User's Order data
+    const response = await fetch("/api/user/order", {
+        method: 'post',
+        body: JSON.stringify({
+            product_id
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    // if response is ok, remove modal from screen
+    if (response.ok) {
+        modal.classList.remove('invisible');
+        modal_title.innerHTML = 'The item is added to your order list.';
+    } else {
+        alert(response.statusText)
+    }
+
+}
+
 async function addWishlist(event) {
     event.preventDefault();
 
@@ -22,4 +54,5 @@ async function addWishlist(event) {
     }
 }
 
+document.querySelector("#orderProductBtn").addEventListener('click', orderFunction)
 document.querySelector('#addWishlistBtn').addEventListener('click', addWishlist);
