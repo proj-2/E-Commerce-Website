9ca006263d60d83c841ceb7d1c0c08e4b370ba56
Yurippe <yurichikawa1992@gmail.com>
2022-06-10 13:21:24 -0400

    add ability to pop up removal confirmation modal

--- public/javascript/delete-wishlist.js
+++ public/javascript/delete-wishlist.js
@@ -1,9 +1,22 @@
-async function deleteWishlistItem(event) {
+async function deleteWishlistConf(event) {
   event.preventDefault();
+
+  const modal = document.querySelector('#conf-modal');
+  const conf_msg = document.querySelector('#conf-msg');
+  const ok_btn = document.querySelector('#conf-ok-btn');
 
   const itemEl = this.parentNode.parentNode;
   const product_id = itemEl.getAttribute('data-id');
   console.log(product_id);
+
+  modal.classList.remove('invisible');
+  conf_msg.innerHTML = 'Are you sure you want to remove the item from the wishlist?';
+  ok_btn.setAttribute('onclick', `deleteWishlistItem(${product_id})`);
+}
+
+async function deleteWishlistItem(product_id) {
+  const modal = document.querySelector('#conf-modal');
+  const ok_btn = document.querySelector('#conf-ok-btn');
 
   const response = await fetch("/wishlist/delete", {
     method: "post",
@@ -13,9 +26,11 @@ async function deleteWishlistItem(event) {
     headers: { "Content-Type": "application/json" },
   });
 
+  ok_btn.removeAttribute('onclick');
+  modal.classList.add('invisible');
+
   if (response.ok) {
     window.location.reload();
-    alert("Item Removed from Wishlist");
   } else {
     alert(response.statusText);
   }
@@ -24,5 +39,5 @@ async function deleteWishlistItem(event) {
 const deleteButtons = document.querySelectorAll("#wishlistDeleteBtn");
 
 for (let i = 0; i < deleteButtons.length; i++) {
-  deleteButtons[i].addEventListener("click", deleteWishlistItem);
+  deleteButtons[i].addEventListener("click", deleteWishlistConf);
 }
