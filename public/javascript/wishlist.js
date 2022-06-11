42f3658028b68857c40d71699d129dead08ae52a
Yurippe <yurichikawa1992@gmail.com>
2022-06-11 10:44:17 -0400

    refactor wishlist.js file

--- public/javascript/wishlist.js
+++ public/javascript/wishlist.js
@@ -1,23 +1,30 @@
+// confirm is the user really want to remove the item
 async function deleteWishlistConf(event) {
   event.preventDefault();
 
+  // get modal elements
   const modal = document.querySelector('#conf-modal');
   const conf_msg = document.querySelector('#conf-msg');
   const ok_btn = document.querySelector('#conf-ok-btn');
 
-  const itemEl = this.parentNode.parentNode;
-  const product_id = itemEl.getAttribute('data-id');
-  console.log(product_id);
+  // get product id from the button element
+  const product_id = this.getAttribute('data-id');
 
+  // pop up confirmation modal
   modal.classList.remove('invisible');
   conf_msg.innerHTML = 'Are you sure you want to remove the item from the wishlist?';
+
+  // add onclick attribute to process delete wishlist item function if clicked ok
   ok_btn.setAttribute('onclick', `deleteWishlistItem(${product_id})`);
 }
 
+// when OK in the confirmation modal is clicked, process the removal
 async function deleteWishlistItem(product_id) {
+  // get modal elements
   const modal = document.querySelector('#conf-modal');
   const ok_btn = document.querySelector('#conf-ok-btn');
 
+  // fetch orderDelete to delete the item from Wishlist data
   const response = await fetch("/wishlist/delete", {
     method: "post",
     body: JSON.stringify({
@@ -26,9 +33,11 @@ async function deleteWishlistItem(product_id) {
     headers: { "Content-Type": "application/json" },
   });
 
+  // remove modal off the screen and remove the onclick attribute
   ok_btn.removeAttribute('onclick');
   modal.classList.add('invisible');
 
+  // if response is ok, refresh the page
   if (response.ok) {
     window.location.reload();
   } else {
@@ -36,8 +45,10 @@ async function deleteWishlistItem(product_id) {
   }
 }
 
+// get all the remove item buttons
 const deleteButtons = document.querySelectorAll("#wishlistDeleteBtn");
 
+// if any of the button is clicked it will perform with its info stored
 for (let i = 0; i < deleteButtons.length; i++) {
   deleteButtons[i].addEventListener("click", deleteWishlistConf);
 }
